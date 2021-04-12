40c09d067527816800787c441258c0ac9e5472d7
Saebom An <joyan828@gmail.com>
2021-04-12 23:15:33 +0900

    Make a small component with ts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import './App.css';
+import '../styles/App.css';
 
-function App() {
+const App: React.FC = () => {
+  const onClick = (name: string) => {
+    console.log(`${name} says hello`)
+  }
+
   return (
     <div className="App">
       React typescript practice
+      <Greetings name='Saebom' onClick={onClick}/>
     </div>
   );
+}
+
+type GreetingsProps = {
+  name: string,
+  mark: string,
+  optional?: string //optional props
+  onClick: (name: string) => void //return nothing
+}; 
+
+// FC는 사용은 권장하지 않는다(defaultProps를 받지 못함)
+const Greetings = ({name, mark, optional, onClick}: GreetingsProps) => {
+  const handleClick = () => onClick(name)
+
+  return <div>
+    HELLO, {name} {mark}
+    { optional && <p>{optional}</p>}
+    <button onClick={handleClick}>Click ME</button>
+  </div>
+}
+
+Greetings.defaultProps = {
+  mark: '!'
 }
 
 export default App;
