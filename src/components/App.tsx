afbb89a059027ac5577fe3276ec6ea28af3483e5
Saebom An <joyan828@gmail.com>
2021-04-13 23:42:25 +0900

    Change ui for todo list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,39 +1,20 @@
 import React from 'react';
-import '../styles/App.css';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './TodoTemplate'
 
-const App: React.FC = () => {
-  const onClick = (name: string) => {
-    console.log(`${name} says hello`)
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
   }
+`
 
+function App() {
   return (
-    <div className="App">
-      React typescript practice
-      <Greetings name='Saebom' onClick={onClick}/>
-    </div>
-  );
-}
-
-type GreetingsProps = {
-  name: string,
-  mark: string,
-  optional?: string //optional props
-  onClick: (name: string) => void //return nothing
-}; 
-
-// FC는 사용은 권장하지 않는다(defaultProps를 받지 못함)
-const Greetings = ({name, mark, optional, onClick}: GreetingsProps) => {
-  const handleClick = () => onClick(name)
-
-  return <div>
-    HELLO, {name} {mark}
-    { optional && <p>{optional}</p>}
-    <button onClick={handleClick}>Click ME</button>
-  </div>
-}
-
-Greetings.defaultProps = {
-  mark: '!'
+    <> 
+      <GlobalStyle />
+      <TodoTemplate>Todo list</TodoTemplate>
+    </>
+  )
 }
 
 export default App;
